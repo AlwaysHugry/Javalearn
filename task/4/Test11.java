29a3eac48b314ef9ff94edc5e15e159c877ce1cf
AlwaysHugry <1279324407@qq.com>
2020-03-22 13:53:34 +0800

    完善第二次作业

--- task/4/Test11.java
+++ task/4/Test11.java
@@ -2,18 +2,18 @@ import java.util.Scanner;
 public class Test11{
     static void add(double a,double b,double c,double d)
     {
-        System.out.println((a+c)+"i+"+(b+d));
+        System.out.println((a+c)+"i+("+(b+d)+")");
     }
     static void subtract(double a,double b,double c,double d)
     {
-        System.out.println((a-c)+"i+"+(b-d));
+        System.out.println((a-c)+"i+("+(b-d)+")");
     }
     static void mutiply(double a,double b,double c,double d)
     {
-        System.out.println((a*d+b*c)+"i+"+(b*d-a*c));
+        System.out.println((a*d+b*c)+"i+("+(b*d-a*c)+")");
     }
     public static void main(String args[])
-    {
+    {1
         double a,b,c,d;
         Scanner input =new Scanner(System.in);
         a=input.nextDouble();
--- task/4/Test13.java
+++ task/4/Test13.java
@@ -1,10 +1,10 @@
-package Test13;
+import student.Student;
 class Test13{
     public static void main(String args[]){
         Student a=new Student(1,3,"Lucre","boy",20);
         Student b=new Student(23,5,"Kiwis","boy",20);
         Student c=new Student(24,5,"XiaoWu","boy",20);
-        System.out.println(a.getId()+"+a.getClassId()+','+a.getName()+','+a.getSex()+','+a.getAge());
+        System.out.println(a.getId()+","+a.getClassId()+','+a.getName()+','+a.getSex()+','+a.getAge());
         a.setAge(19);
         a.setName("Panda");
         a.setSex("girl");
